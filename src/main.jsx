f791b4327122c0946be5fc4517e0b7203026e153
JUAN manuel calleros <116044083+manuelcalso@users.noreply.github.com>
2023-09-09 12:05:09 -0600

    primer commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Esta es la pagina de inicio</p>,
+  },
+  {
+    path: "/login",
+    element: <p>Este es mi pagina de inicio de sesion</p>,
+  },
+  {
+    path: "/app",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
